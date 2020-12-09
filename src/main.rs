a4eaf3e7903fca100139f707601f5271c36cfc35
Peter Malmgren <ptmalmgren@gmail.com>
2020-12-09 16:44:07 -0500

    Top and bottom collisions of bricks work

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::process;
 use std::{thread, time};
 use std::cmp;
 use std::time::{SystemTime, UNIX_EPOCH};
+use std::convert::TryFrom;
 
 // Should be divisible by 3 for [left, center, right]
 const PADDLE_WIDTH: i32 = 12;
@@ -45,6 +46,13 @@ struct Point {
     y: i32,
 }
 
+impl Point {
+    fn collision(&self, bounds: &Bounds, vel: &Point) -> bool {
+        // every object in our game has a height of 1
+        (self.y + vel.y) == bounds.max_y && (self.x + vel.x) <= bounds.max_x && (self.x + vel.x) >= bounds.min_x
+    }
+}
+
 struct Bounds {
     min_x: i32,
     min_y: i32,
@@ -58,6 +66,10 @@ enum MoveResult {
     HitPaddleCenter,
     HitPaddleLeft,
     HitPaddleRight,
+    HitBrickBottom(i32),
+    HitBrickTop(i32),
+    HitBrickLeft(i32),
+    HitBrickRight(i32),
 }
 
 // Can be a ball, a paddle, or a brick.
@@ -69,57 +81,88 @@ struct GameObject {
 }
 
 impl GameObject {
-    fn get_bounds(&mut self) -> Bounds {
+    fn get_bounds(&self) -> Bounds {
         let left_edge = self.pos.x - (self.width / 2);
         let right_edge = self.pos.x + (self.width / 2);
         Bounds { min_x: left_edge, max_x: right_edge, min_y: self.pos.y, max_y: self.pos.y }
     }
-    // moves the game object by the direction and returns a collision
-    fn push(&mut self, direction: Direction, bounds: &Bounds, object: Option<&Bounds>) -> Option<MoveResult> {
+    // moves the game object by the direction and returns a collision, if any
+    fn move1(&mut self, direction: Direction, bounds: &Bounds, paddle_bounds: Option<&Bounds>, bricks: Option<&Vec<Bounds>>) -> Option<MoveResult> {
         let left_edge = self.pos.x - (self.width / 2);
         let right_edge = self.pos.x + (self.width / 2);
         return match direction {
             Direction::Left => {
-                if left_edge > 1 {
-                    self.pos.x -= 1;
-                    return None;
+                if left_edge <= 1 {
+                    return Some(MoveResult::HitWallLeftRight);
                 }
-                Some(MoveResult::HitWallLeftRight)
-            }
+
+                self.pos.x -= 1;
+                None
+            },
             Direction::Right => {
-                if right_edge < (bounds.max_x - 2) {
-                    self.pos.x += 1;
-                    return None;
+                if right_edge >= (bounds.max_x - 2) {
+                    return Some(MoveResult::HitWallLeftRight)
                 }
-                Some(MoveResult::HitWallLeftRight)
+                self.pos.x += 1;
+                None
             },
             Direction::Up => {
-                if self.pos.y > 1 {
-                    self.pos.y -= 1;
-                    return None;
+                if self.pos.y <= 1 {
+                    return Some(MoveResult::HitWallTopBottom);
                 }
-                Some(MoveResult::HitWallTopBottom)
+
+                return match bricks {
+                    None => {
+                        self.pos.y -= 1;
+                        None
+                    },
+                    Some(bricks) => {
+                        for (idx, brick_bounds) in bricks.iter().enumerate() {
+                            if self.pos.collision(brick_bounds, &self.vel) {
+                                return Some(MoveResult::HitBrickBottom(idx as i32));
+                            }
+                        }
+                        self.pos.y -= 1;
+                        None
+                    }
+                }
             },
             Direction::Down => {
                 if self.pos.y >= (bounds.max_y - 2) {
                     return Some(MoveResult::HitWallTopBottom)
                 }
 
-                return match object {
+                let paddle_collision = match paddle_bounds {
+                    None => None,
+                    Some(paddle_bounds) => {
+                        if self.pos.collision(paddle_bounds, &self.vel) {
+                            let third = PADDLE_WIDTH / 3;
+                            if self.pos.x < (paddle_bounds.min_x + third) {
+                                return Some(MoveResult::HitPaddleLeft);
+                            }
+                            if self.pos.x < (paddle_bounds.min_x + (2 * third)) {
+                                return Some(MoveResult::HitPaddleCenter);
+                            }
+                            return Some(MoveResult::HitPaddleRight);
+                        }
+                        None
+                    }
+                };
+
+                if paddle_collision.is_some() {
+                    return paddle_collision;
+                }
+
+                return match bricks {
                     None => {
                         self.pos.y += 1;
                         None
                     },
-                    Some(object) => {
-                        if self.pos.y == object.max_y && self.pos.x < object.max_x && self.pos.x > object.min_x {
-                            let third = PADDLE_WIDTH / 3;
-                            if self.pos.x < (object.min_x + third) {
-                                return Some(MoveResult::HitPaddleLeft);
+                    Some(bricks) => {
+                        for (idx, brick_bounds) in bricks.iter().enumerate() {
+                            if self.pos.collision(brick_bounds, &self.vel) {
+                                return Some(MoveResult::HitBrickTop(idx as i32));
                             }
-                            if self.pos.x < (object.min_x + (2 * third)) {
-                                return Some(MoveResult::HitPaddleCenter);
-                            }
-                            return Some(MoveResult::HitPaddleRight);
                         }
                         self.pos.y += 1;
                         None
@@ -131,15 +174,16 @@ impl GameObject {
     }
 
     // floats the game object by the velocity
-    fn float(&mut self, bounds: &Bounds, paddle: &Bounds) {
+    fn float(&mut self, screen_bounds: &Bounds, paddle_bounds: &Bounds, brick_bounds: &Vec<Bounds>) -> Option<i32> {
+        let mut hit_brick: Option<i32> = None;
         let x_collision: Option<MoveResult> = match self.vel.x {
-            x if x < 0 => self.push(Direction::Left, bounds, Some(paddle)),
-            x if x > 0 => self.push(Direction::Right, bounds, Some(paddle)),
+            x if x < 0 => self.move1(Direction::Left, screen_bounds, Some(paddle_bounds), Some(brick_bounds)),
+            x if x > 0 => self.move1(Direction::Right, screen_bounds, Some(paddle_bounds), Some(brick_bounds)),
             _ => None,
         };
         let y_collision: Option<MoveResult> = match self.vel.y {
-            y if y > 0 => self.push(Direction::Down, bounds, Some(paddle)),
-            y if y < 0 => self.push(Direction::Up, bounds, Some(paddle)),
+            y if y > 0 => self.move1(Direction::Down, screen_bounds, Some(paddle_bounds), Some(brick_bounds)),
+            y if y < 0 => self.move1(Direction::Up, screen_bounds, Some(paddle_bounds), Some(brick_bounds)),
             _ => None,
         };
 
@@ -158,14 +202,40 @@ impl GameObject {
             },
             Some(MoveResult::HitWallTopBottom) => self.vel.y = -self.vel.y,
             Some(MoveResult::HitWallLeftRight) => self.vel.x = -self.vel.x,
+            Some(MoveResult::HitBrickBottom(brick_idx)) => {
+                self.vel.y = -self.vel.y;
+                hit_brick = Some(brick_idx);
+            },
+            Some(MoveResult::HitBrickTop(brick_idx)) => {
+                self.vel.y = -self.vel.y;
+                hit_brick = Some(brick_idx);
+            },
+            Some(MoveResult::HitBrickLeft(brick_idx)) => {
+                self.vel.x = -self.vel.x;
+                hit_brick = Some(brick_idx);
+            },
+            Some(MoveResult::HitBrickRight(brick_idx)) => {
+                self.vel.x = -self.vel.x;
+                hit_brick = Some(brick_idx);
+            },
+            _ => (),
             None => (),
         };
 
         match x_collision {
+            Some(MoveResult::HitBrickLeft(brick_idx)) => {
+                self.vel.x = -self.vel.x;
+                hit_brick = Some(brick_idx);
+            },
+            Some(MoveResult::HitBrickRight(brick_idx)) => {
+                self.vel.y = -self.vel.x;
+                hit_brick = Some(brick_idx);
+            },
             Some(_collision) => self.vel.x = -self.vel.x,
             None => (),
         };
 
+        return hit_brick;
     }
 
     fn draw(&self) {
@@ -189,6 +259,7 @@ struct Game {
     bounds: Bounds,
     player: GameObject,
     ball: GameObject,
+    bricks: Vec<GameObject>,
     window: WINDOW,
     last_ball_move: u128,
 }
@@ -204,20 +275,42 @@ impl Game {
         self.ball.draw();
     }
 
+    fn draw_bricks(&mut self) {
+        for brick in &self.bricks {
+            brick.draw()
+        }
+    }
+
     fn move_player(&mut self, direction: Direction) {
         self.player.clear();
-        self.player.push(direction, &self.bounds, None);
+        self.player.move1(direction, &self.bounds, None, None);
         self.draw_player();
     }
 
-    fn move_ball(&mut self) {
+    fn move_ball(&mut self) -> Option<i32> {
         let now = now_ms();
+        let bounds = self.get_brick_bounds();
         if now - self.last_ball_move > 70 {
             self.last_ball_move = now;
             self.ball.clear();
-            self.ball.float(&self.bounds, &self.player.get_bounds());
+            let hit_brick = self.ball.float(&self.bounds, &self.player.get_bounds(), &bounds);
             self.draw_ball();
+            return hit_brick;
         }
+        None
+    }
+
+    fn get_brick_bounds(&self) -> Vec<Bounds> {
+        let mut brick_bounds = Vec::with_capacity(self.bricks.len());
+        for brick in &self.bricks {
+            let bounds = brick.get_bounds();
+            brick_bounds.push(bounds);         
+        }
+        brick_bounds
+    }
+
+    fn rm_brick(&mut self, brick_idx: i32) {
+
     }
 }
 
@@ -250,6 +343,7 @@ fn init() -> Result<WINDOW, String> {
 fn run(game: &mut Game) {
     let ten_millis = time::Duration::from_millis(10);
  
+    game.draw_bricks();
     game.draw_player();
     game.draw_ball();
     refresh();
@@ -262,7 +356,11 @@ fn run(game: &mut Game) {
             },
             Command::Quit => break,
         };
-        game.move_ball();
+        let hit_brick = game.move_ball();
+        match hit_brick {
+            Some(brick_idx) => game.rm_brick(brick_idx),
+            None => (),
+        }
         refresh();
     }
 }
@@ -287,9 +385,30 @@ fn main() {
     let mut max_y: i32 = 0;
     getmaxyx(window, &mut max_y, &mut max_x);
 
+    let brick_width = max_x / 8;
+    let capacity = usize::try_from(5 * 8).unwrap();
+    let mut bricks = Vec::with_capacity(capacity);
+    for row in 1..6 {
+        let offset = match (row % 2) {
+            0 => 5,
+            _ => 13,
+        };
+        for col in 0..7 {
+            bricks.push(
+                GameObject {
+                    pos: Point { x: offset + (col * brick_width) + (brick_width / 2), y: row },
+                    vel: Point { x: 0, y: 0 },
+                    disp_char: col.to_string().chars().nth(0).unwrap() as u32,
+                    width: brick_width,
+                }
+            );
+        }
+    }
+
     let mut game = Game {
         window: window,
         bounds: Bounds { max_x: max_x, max_y: max_y, min_x: 0, min_y: 0 },
+        // we want the paddle to be above the bottom border of the screen
         player: GameObject {
             pos: Point { x: (max_x / 2), y: max_y - 4},
             vel: Point { x: 0, y: 0 },
@@ -297,11 +416,12 @@ fn main() {
             width: PADDLE_WIDTH,
         },
         ball: GameObject {
-            pos: Point { x: (max_x / 2), y: 2 },
+            pos: Point { x: (max_x / 2), y: 7 },
             vel: Point { x: 0, y: 1 },
-            disp_char: '*' as u32,
+            disp_char: '0' as u32,
             width: 1,
         },
+        bricks: bricks,
         last_ball_move: now_ms()
     };
     
